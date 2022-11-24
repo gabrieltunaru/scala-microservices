ee3688927918d4d260e85cab086bc88146a5e883
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2022-11-24 17:57:14 +0200

    Auth: move business logic to algebra

--- auth/src/main/scala/com/cannondev/authscala3/Authscala3Routes.scala
+++ auth/src/main/scala/com/cannondev/authscala3/Authscala3Routes.scala
@@ -5,11 +5,6 @@ import cats.data.{EitherT, OptionT}
 import cats.effect.{IO, Resource, Sync}
 import cats.effect.kernel.Concurrent
 import cats.implicits.*
-import com.cannondev.authscala3.AuthInfo.User
-import com.cannondev.authscala3.config.DbConfig.AppConfig
-import com.cannondev.authscala3.errors.{InvalidToken, MissingHeader, UserNotFound, WrongPassword}
-import com.cannondev.authscala3.storage.daos.{UserModel, UserRepository}
-import com.cannondev.authscala3.util.OptionUtil._
 import org.http4s.Status.{BadRequest, Forbidden, NotFound, Ok, Unauthorized}
 import org.http4s.{EntityDecoder, Headers, HttpRoutes, Response}
 import org.http4s.circe.jsonOf
@@ -18,29 +13,24 @@ import skunk.Session
 import tsec.common.{VerificationFailed, Verified}
 import tsec.passwordhashers.{PasswordHash, PasswordHasher}
 import tsec.passwordhashers.jca.BCrypt
-import util.Jwt
-import util.Jwt.Token
 import io.circe.generic.auto.*
 import io.circe.syntax.*
 import org.http4s.headers.Authorization
 
+import com.cannondev.authscala3.AuthInfo.User
+import com.cannondev.authscala3.config.DbConfig.AppConfig
+import com.cannondev.authscala3.errors.*
+import com.cannondev.authscala3.storage.daos.{UserModel, UserRepository}
+import com.cannondev.authscala3.util.OptionUtil._
+import com.cannondev.authscala3.algebra.UserAlgebra
+import util.Jwt
+import util.Jwt.Token
+
 object Authscala3Routes {
 
-  private def checkPassword[F[_]](user: User, dbUserO: Option[UserModel])(implicit
-      hasher: PasswordHasher[F, BCrypt],
-      F: Concurrent[F]
-  ): F[Boolean] = {
-    dbUserO match {
-      case Some(dbUser) =>
-        for {
-          isPasswordRight <- BCrypt.checkpwBool[F](user.password, PasswordHash(dbUser.password))
-          res <- if (isPasswordRight) F.pure(true) else F.raiseError(WrongPassword(user.username))
-        } yield res
-      case None => F.raiseError(UserNotFound(user.username))
-    }
-  }
-
-  private def getAuthToken[F[_]](headers: Headers)(implicit F: Concurrent[F]): F[String] = {
+  private def getAuthToken[F[_]](
+      headers: Headers
+  )(implicit F: Concurrent[F]): F[String] = {
     headers.get(Authorization.name) match {
       case Some(value) => F.pure(value.head.value)
       case None        => F.raiseError(MissingHeader)
@@ -48,38 +38,33 @@ object Authscala3Routes {
   }
 
   def registerRoute[F[_]: Concurrent](cfg: AppConfig)(implicit
+      userAlgebra: UserAlgebra[F],
       session: Resource[F, Session[F]],
       hasher: PasswordHasher[F, BCrypt]
   ): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F] {}
     import dsl._
-    implicit val decoder: EntityDecoder[F, AuthInfo.User] = jsonOf[F, AuthInfo.User]
+    implicit val decoder: EntityDecoder[F, AuthInfo.User] =
+      jsonOf[F, AuthInfo.User]
     import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 
     HttpRoutes.of[F] {
       case req @ POST -> Root / "register" =>
         for {
-          user <- req.as[AuthInfo.User]
-          pwHash <- BCrypt.hashpw[F](user.password)
-          userWithHashedPassword = UserModel(username = user.username, password = pwHash)
-          _ <- UserRepository().insert(userWithHashedPassword)
-          insertedUserO <- UserRepository().find(user.username)
-          insertedUser <- insertedUserO.orElse(UserNotFound(user.username))
-          token = Token(Jwt.encode(cfg.hasingPrivateKey, insertedUser.uuid.toString())).asJson
-          res <- Ok(token)
+          credentials <- req.as[User]
+          token <- userAlgebra.register(credentials)
+          res <- Ok(token.asJson)
         } yield res
       case req @ POST -> Root / "login" =>
         val result = for {
-          user <- req.as[AuthInfo.User]
-          dbUserO <- UserRepository().find(user.username)
-          passwordResult <- checkPassword(user, dbUserO)
-          dbUser <- dbUserO.orElse(UserNotFound(user.username))
-          token = Token(Jwt.encode(cfg.hasingPrivateKey, dbUserO.get.uuid.toString)).asJson
-          res <- Ok(token)
+          credentials <- req.as[User]
+          token <- userAlgebra.login(credentials)
+          res <- Ok(token.asJson)
         } yield res
         result.recoverWith {
-          case UserNotFound(username)  => BadRequest(s"User $username not found")
-          case WrongPassword(username) => BadRequest(s"Wrong password for user $username")
+          case UserNotFound(username) => BadRequest(s"User $username not found")
+          case WrongPassword(username) =>
+            BadRequest(s"Wrong password for user $username")
         }
       case req @ POST -> Root / "validate" =>
         val result = for {
---
+++ auth/src/main/scala/com/cannondev/authscala3/algebra/UserAlgebra.scala
@@ -0,0 +1,71 @@
+package com.cannondev.authscala3.algebra
+
+import com.cannondev.authscala3.AuthInfo
+import cats.effect.IO
+import tsec.passwordhashers.jca.BCrypt
+import cats.effect.kernel.Resource
+import tsec.passwordhashers.PasswordHasher
+import skunk.Session
+import cats.implicits.*
+import cats.effect.kernel.Async
+import com.cannondev.authscala3.storage.daos.UserModel
+import com.cannondev.authscala3.storage.daos.UserRepository
+import com.cannondev.authscala3.errors.*
+import com.cannondev.authscala3.util.Jwt.Token
+import com.cannondev.authscala3.util.Jwt
+import com.cannondev.authscala3.config.DbConfig.AppConfig
+import io.circe.syntax.*
+import io.circe.generic.auto.*
+
+import com.cannondev.authscala3.AuthInfo.User
+import tsec.passwordhashers.PasswordHash
+import com.cannondev.authscala3.util.OptionUtil._
+
+class UserAlgebra[F[_]](implicit
+    session: Resource[F, Session[F]],
+    cfg: AppConfig,
+    F: Async[F]
+) {
+
+  private def checkPassword(
+      user: User,
+      dbUserO: Option[UserModel]
+  ): F[Boolean] = {
+    dbUserO match {
+      case Some(dbUser) =>
+        for {
+          isPasswordRight <- BCrypt
+            .checkpwBool[F](user.password, PasswordHash(dbUser.password))
+          res <-
+            if (isPasswordRight) F.pure(true)
+            else F.raiseError(WrongPassword(user.username))
+        } yield res
+      case None => F.raiseError(UserNotFound(user.username))
+    }
+  }
+
+  def register(credentials: User): F[Token] = for {
+    pwHash <- BCrypt.hashpw[F](credentials.password)
+    userWithHashedPassword = UserModel(
+      username = credentials.username,
+      password = pwHash
+    )
+    _ <- UserRepository().insert(userWithHashedPassword)
+    insertedUserO <- UserRepository().find(credentials.username)
+    insertedUser: UserModel <- insertedUserO match {
+      case Some(value) => F.pure(value)
+      case None        => F.raiseError(UserNotFound(credentials.username))
+    }
+  } yield Token(
+    Jwt.encode(cfg.hasingPrivateKey, insertedUser.uuid.toString())
+  )
+
+  def login(credentials: User): F[Token] = for {
+    dbUserO <- UserRepository().find(credentials.username)
+    passwordResult <- checkPassword(credentials, dbUserO)
+    dbUser <- dbUserO.orElse(UserNotFound(credentials.username))
+  } yield Token(
+    Jwt.encode(cfg.hasingPrivateKey, dbUser.uuid.toString)
+  )
+
+}
